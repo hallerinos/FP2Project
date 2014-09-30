b374ce251deaac2b112dedaf072f8dc62e027ee4
halleritis <halleritis@gmail.com>
2014-09-30 11:24:38 +0200

    Testing two different ways for dismissing the std:: keyword

--- exercise/ch2/cout_test.cpp
+++ exercise/ch2/cout_test.cpp
@@ -5,9 +5,20 @@
 
 int main()
 {
-	std::cout << "Is it buggy or not...?" << std::endl; 
-	std::cout << 8+5 << std::endl;
-	std::cout << (8+5) << std::endl;
-	std::cout << "Nope.";
-	return 0;
+//	std::cout << "Is it buggy or not...?" << std::endl; 
+//	std::cout << 8+5 << std::endl;
+//	std::cout << (8+5) << std::endl;
+//	std::cout << "Nope.";
+//	return 0;
+
+//  Way #1 for dismissing std::
+//	using std::cout;
+//	using std::endl;
+
+//  Way #2 for dismissing std::
+	using namespace std;
+	cout << "Is it buggy or not...?\t\t" << std::endl;
+	cout << "\t\t\t " << 8+5 << endl;
+	cout << "\t\t\t " << (8+5) << endl;
+	cout << "\t\t\t ...nope.";
 }
