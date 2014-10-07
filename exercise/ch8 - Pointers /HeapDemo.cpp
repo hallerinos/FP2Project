3e06629dfd4dfc1dd446704c08f6c276ee77461c
halleritis <halleritis@gmail.com>
2014-10-07 07:42:23 +0200

    Added ch8 stuff

---
+++ exercise/ch8 - Pointers /HeapDemo.cpp
@@ -0,0 +1,27 @@
+# include <iostream>
+
+using namespace std;
+
+int main() 
+{
+	int localVariable = 5;
+	int *pLocal = &localVariable;
+	int *pHeap = new int;
+
+	*pHeap = 7;
+
+	cout << "localVariable: " << localVariable << endl;
+	cout << "*pLocal: " << *pLocal << endl;
+	cout << "*pHeap: " << *pHeap << endl << endl;
+
+	delete pHeap;
+
+	pHeap = new int;
+	*pHeap = 9;
+
+	cout << "After delete, reassigning *pHeap = 9..." << endl;
+	cout << "*pHeap: " << *pHeap << endl;
+	delete pHeap;
+	
+	return 0;
+}
