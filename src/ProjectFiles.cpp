2b61eaf0a7b377e01b1c3dc78f448d1d6d952db7
wjanke <janke4592@gmx.de>
2014-10-11 19:55:25 +0200

    stuff

--- src/ProjectFiles.cpp
+++ src/ProjectFiles.cpp
@@ -1,15 +1,59 @@
-//============================================================================
-// Name        : ProjectFiles.cpp
-// Author      : Willi
-// Version     :
-// Copyright   : Meiins
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
 #include <iostream>
+#include <time.h>
+#include <sys/time.h>
+#include <stdlib.h>
+
+
+
 using namespace std;
 
-int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
+int main()
+{
+	srand(time(NULL));
+
+	int n,random,i,j;
+	cout << "Number of Vectors: ";
+	cin >> n;
+
+	struct timeval start, end;
+	gettimeofday(&start,NULL);
+
+	int *pVectors = new int[3*n];
+	int *pI = pVectors;
+	int sum=0;
+
+	for (i = 0; i < 3*n; pI++, i++ ){
+		*pI = rand() % 101 - 50;
+	}
+
+	pI = pVectors;
+
+	for (i = 0; i < 3*n; i++, pI++){
+		sum = sum + *pI;
+	}
+
+	cout << "Sum: " << sum << endl;
+
+/*	int vectors[n][3], sum[3] = {0,0,0};
+	for (i=0; i < n; i++){
+		for (j=0; j<3; j++){
+			vectors[i][j]=rand() % 101 - 50;
+		}
+	}
+	for (i=0; i < 3; i++){
+		for (j=0; j<n; j++){
+			sum[i] = sum[i] + vectors[j][i];
+		}
+	}
+	cout << "SumX: " << sum[0] << "  SumY: " << sum[1] << "  SumZ: " << sum[2] << endl;
+	float result = sum[0]+sum[1]+sum[2];
+	cout << "Sum: " << result << endl;*/
+
+
+    gettimeofday(&end,NULL);
+    cout << (float) ((end.tv_sec - start.tv_sec)*1000000 + end.tv_usec - start.tv_usec) / 1000000.0 << endl;
 	return 0;
 }
+
+
