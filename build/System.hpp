805a75d60ae93066aa855a9f894c9840c5a1ddb7
William <wjanke@students.uni-mainz.de>
2014-10-16 13:37:11 +0200

    Tidied up a little more

--- build/System.hpp
+++ build/System.hpp
@@ -42,28 +42,23 @@ class System {
  *------------------------------------------------------------------*/
 System::System( int newNumberOfParticles, int newDimOfSystem, 
 		double newTempOfSystem, int newSizeOfSys) {
+
 	tempOfSystem = newTempOfSystem;
 	numberOfParticles = newNumberOfParticles;
 	dimOfSystem = newDimOfSystem;
 	sizeOfSys = newSizeOfSys;
 	MIN_CUTOFF = pow( 10, -6 );
 	MAX_CUTOFF = sizeOfSys;
-	
 	forces = 0;
 	forces2 = 0;
 	velos = 0;
 	coords = new double[ numberOfParticles * dimOfSystem ];
-	// cout << "These are the " << numberOfParticles 
-	// << " Particles:" << endl;
+
+	//Initialization of the particle locations
 	for ( int i = 0; i < numberOfParticles; i++)
- 		for ( int j = 0; j < dimOfSystem; j++ )	{
+ 		for ( int j = 0; j < dimOfSystem; j++ )	
 			(coords)[i*dimOfSystem + j] = (float)rand() / INT_MAX * sizeOfSys;
-			// (coords)[i*dimOfSystem + j] = i*dimOfSystem + j;
-			// cout << "Particle: " << i << " Axis: " << j << ": \t" 
-			// << (coords)[i*dimOfSystem + j] << "\t" << endl;
-		}
-	cout << endl;
-}
+	}
 
 System::~System() { 
 	delete coords;
@@ -84,6 +79,7 @@ int System::GetCoordinate ( int partNumber, int axis) const {
 
 System::System( int newNumberOfParticles, int newDimOfSystem, 
 		double newTempOfSystem, int newSizeOfSys, float newMass) {
+
 	tempOfSystem = newTempOfSystem;
 	numberOfParticles = newNumberOfParticles;
 	dimOfSystem = newDimOfSystem;
@@ -91,20 +87,15 @@ System::System( int newNumberOfParticles, int newDimOfSystem,
 	mass = newMass;
 	MIN_CUTOFF = pow( 10, -12 );
 	MAX_CUTOFF = sizeOfSys/2;
-
 	forces2 = new double[ numberOfParticles * dimOfSystem ];
 	forces = new double[ numberOfParticles * dimOfSystem ];
 	velos = new double[ numberOfParticles * dimOfSystem ];
 	coords = new double[ numberOfParticles * dimOfSystem ];
-	// cout << "These are the " << numberOfParticles 
-	// << " Particles:" << endl;
+
+	// Initialization of particle locations
 	for ( int i = 0; i < numberOfParticles; i++)
- 		for ( int j = 0; j < dimOfSystem; j++ )	{
+ 		for ( int j = 0; j < dimOfSystem; j++ )
 			(coords)[i*dimOfSystem + j] = (float) rand() / INT_MAX * sizeOfSys ;
-			// (coords)[i*dimOfSystem + j] = i*dimOfSystem + j;
-			// cout << "Particle: " << i << " Axis: " << j << ": \t" 
-			// << (coords)[i*dimOfSystem + j] << "\t" << endl;
-		}
 
 	//Initalization of Velocities
 	float vmax =  sqrt(dimOfSystem * tempOfSystem / mass) ; // k_B=1
@@ -115,64 +106,42 @@ System::System( int newNumberOfParticles, int newDimOfSystem,
 			(velos)[(i+1)*dimOfSystem + j] = -(velos)[i*dimOfSystem + j];
 		}
 
-	/*
-	double sum=0;
-
-	for ( int i = 0; i < numberOfParticles*dimOfSystem; i++){
-		sum+= velos[i];
-	}
-	cout << "Sum of Velocities: " << sum << endl;
-	
-	sum=0;
-
-	for ( int i = 0; i < numberOfParticles*dimOfSystem; i++){
-		sum+= pow(velos[i],2);
-	}
-	cout << "vmax: " << vmax << endl;
-
-	cout << "E(v^2): " << sum / numberOfParticles << endl;*/
-
-
 
 	// Calculation of initial Forces
 	
-double rsq;
-double diffV[3];
+	double rsq;
+	double diffV[3];
 
-for ( int i = 0; i < numberOfParticles * dimOfSystem; i++ )
-	forces[i]=0;
+	for ( int i = 0; i < numberOfParticles * dimOfSystem; i++ )
+		forces[i]=0;
 
-for ( int i = 0; i < numberOfParticles; i++ ){
-	for ( int j = i+1; j < numberOfParticles; j++ ){
-		rsq = 0;
-		for ( int k = 0; k < dimOfSystem; k++ ){
-			diffV[k] = coords[i*dimOfSystem + k] - coords[j*dimOfSystem + k];
-			if( diffV[k] > sizeOfSys/2 ) {
-				diffV[k] = diffV[k] - sizeOfSys;
-			} else if ( diffV[k] < - sizeOfSys/2 ) {
-				diffV[k] = diffV[k] + sizeOfSys;
-			}
-			rsq += pow(diffV[k],2);
-		}
+	for ( int i = 0; i < numberOfParticles; i++ ){
+		for ( int j = i+1; j < numberOfParticles; j++ ){
 
-		if ( rsq < MIN_CUTOFF || rsq > MAX_CUTOFF ){} else
+			//Get distance vector and calculate distance-squared between
+			//particles i and j
+			rsq = 0;
 			for ( int k = 0; k < dimOfSystem; k++ ){
-				forces[i*dimOfSystem + k] += 
-					24 * diffV[k] * ( 2/pow(rsq,7) - 1/pow(rsq,4) );
-				forces[j*dimOfSystem + k] -=
-					24 * diffV[k] * ( 2/pow(rsq,7) - 1/pow(rsq,4) );
+				diffV[k] = coords[i*dimOfSystem + k] - coords[j*dimOfSystem + k];
+				if( diffV[k] > sizeOfSys/2 ) {
+					diffV[k] = diffV[k] - sizeOfSys;
+				} else if ( diffV[k] < - sizeOfSys/2 ) {
+					diffV[k] = diffV[k] + sizeOfSys;
+				}
+				rsq += pow(diffV[k],2);
 			}
-	}			
-}
+
+			//Check cutoff distance save force
+			if ( rsq < MIN_CUTOFF || rsq > MAX_CUTOFF ){} else
+				for ( int k = 0; k < dimOfSystem; k++ ){
+					forces[i*dimOfSystem + k] += 
+						24 * diffV[k] * ( 2/pow(rsq,7) - 1/pow(rsq,4) );
+					forces[j*dimOfSystem + k] -=
+						24 * diffV[k] * ( 2/pow(rsq,7) - 1/pow(rsq,4) );
+				}
+		}			
+	}
 	
-/*	cout << "Coords 1: " << coords[0] << "\t" << coords[1] << "\t" << coords[2] << endl;
-	cout << "Coords 2: " << coords[3] << "\t" << coords[4] << "\t" << coords[5] << endl;
-
-	cout << "Forces 1: " << forces[0] << "\t" << forces[1] << "\t" 
-		<< forces[2] << endl;
-	cout << "Forces 2: " << forces[3] << "\t" << forces[4] << "\t" 
-		<< forces[5] << endl;*/
-
 	cout << endl;
 }
 
@@ -181,18 +150,16 @@ for ( int i = 0; i < numberOfParticles; i++ ){
  *------------------------------------------------------------------*/
 double System::GetDistance( int partNumOne, int partNumTwo ) const {
 	double dist = 0;
-	double tmp = 0;
-	double coordOne;
-	double coordTwo;
+	double coordDiff = 0;
 	for ( int j = 0; j < dimOfSystem; j++ ) {
-		coordOne = coords[ partNumOne*dimOfSystem + j];
-		coordTwo = coords[ partNumTwo*dimOfSystem + j];
-		tmp = coordTwo - coordOne;	
-		if( tmp > sizeOfSys/2 ) {
-			dist += pow( tmp - sizeOfSys, 2 );
-		} else if ( tmp < - sizeOfSys/2 ) {
-			dist += pow( tmp + sizeOfSys, 2 );
-		} else dist += pow( tmp, 2 );
+		coordDiff = coords[ partNumTwo*dimOfSystem + j] 
+			- coords[ partNumOne*dimOfSystem + j];
+
+		if( coordDiff > sizeOfSys/2 ) {
+			dist += pow( coordDiff - sizeOfSys, 2 );
+		} else if ( coordDiff < - sizeOfSys/2 ) {
+			dist += pow( coordDiff + sizeOfSys, 2 );
+		} else dist += pow( coordDiff, 2 );
 	}
 	return sqrt( dist );
 }
